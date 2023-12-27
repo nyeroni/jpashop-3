b95796b11e6d71dc148b605836dc987f80f6c772
nyeroni <nir2y@naver.com>
2023-12-27 21:47:11 +0900

    Update: RunWith -> ExtendWith

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class MemberRepositoryTest {
 
@@ -17,7 +17,7 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
-    public void testMember()throws Exception{
+    public void testMember() throws Exception{
         //given
         Member member = new Member();
         member.setUsername("member1");
