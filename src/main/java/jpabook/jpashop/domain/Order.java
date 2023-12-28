84401e008eb75aad5a9bc2e2de1a33afcd386099
nyeroni <nir2y@naver.com>
2023-12-28 15:04:57 +0900

    Update: 오타 수정

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -66,7 +66,7 @@ public class Order {
     /** 주문 취소 */
     public void cancel(){
         if(delivery.getStatus()==DeliveryStatus.COMP){
-            throw new IllegalStateException("이미 배송완료된 상품은 취소할 수 없습니다.")
+            throw new IllegalStateException("이미 배송완료된 상품은 취소할 수 없습니다.");
         }
         this.setStatus(OrderStatus.CANCEL);
         for(OrderItem orderItem : orderItemList){
