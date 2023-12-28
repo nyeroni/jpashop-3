ac0296bc9eae943a5ec725705dfc965c72772652
nyeroni <nir2y@naver.com>
2023-12-28 16:57:27 +0900

    Update: 회원 목록 controller 구현

--- src/main/java/jpabook/jpashop/web/MemberController.java
+++ src/main/java/jpabook/jpashop/web/MemberController.java
@@ -12,6 +12,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -35,4 +37,13 @@ public class MemberController {
         memberService.join(member);
         return "redirect:/";
     }
+
+    @GetMapping(value = "/members")
+    public String list(Model model){
+        List<Member> memberList = memberService.findMembers();
+        model.addAttribute("members",memberList);
+        return "members/memberList";
+    }
+
+
 }
