1d5c59c0922029fbadf2f11faf141755a338cd26
nyeroni <nir2y@naver.com>
2023-12-31 01:06:39 +0900

    Update: 엔티티 직접 노출 버전 생성

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,5 +1,15 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * V1. 엔티티 직접 노출
  * - 엔티티가 변하면 API 스펙이 변함
@@ -10,7 +20,7 @@ package jpabook.jpashop.api;
  * - 트랜잭션 안에서 지연로딩 필요
  *
  * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용 O)
- * - 페이징 시에는 N부분을 포기해야 함(대신에 betch fetch size? 옵션 주면 N -> 1 쿼리로 변경 가능)
+ * - 페이성징 시에는 N부분을 포기해야 함(대신에 betch fetch size? 옵션 주면 N -> 1 쿼리로 변경 가능)
  *
  * V4. JPA에서 DTO로 바로 조회, 컬렉션 N 조회(1 + N Query)
  * - 페이징 가능
@@ -21,5 +31,28 @@ package jpabook.jpashop.api;
  * V6. JPA에서 DTO로 바로 조회, 플랫 데이터(1Query) (1Query)
  * - 페이징 불가능..
  */
+
+@RestController
+@RequiredArgsConstructor
 public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등록, LAZY=null 처리
+     * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getUsername();//Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기화
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream()
+                    .forEach(o -> o.getItem().getName());//Lazy 강제 초기화
+        }
+        return all;
+    }
 }
